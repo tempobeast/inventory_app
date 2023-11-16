6d7164c990e8ec916b73fb678215df00191e25ca
Zach <shaw.zacht@gmail.com>
2023-11-15 23:07:08 -0800

    computer create route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
-include ActionController::Cookies
+    include ActionController::Cookies
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    private
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 
 end
--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -10,4 +10,16 @@ class ComputersController < ApplicationController
         render json: computer, status: :ok
     end
 
+    def create 
+        byebug
+        computer = Computer.create!(computer_params)
+        render json: computer, status: :created
+    end
+
+
+    private
+    
+    def computer_params
+        params.permit(asset_tag: params[:asset_tag], building_id: params[:building_id], model: params[:model], serial_number: params[:serial_number], checkout_status: params[:checkout_status])
+    end
 end
