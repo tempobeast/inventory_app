5298a270d6e9d71101eea976f7ea59fc7ab18db6
Zach <shaw.zacht@gmail.com>
2023-11-16 21:44:39 -0800

    create and destroy methods

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,7 +1,5 @@
 class BuildingsController < ApplicationController
 
-  
-
     def index
         buildings = Building.all
         render json: buildings, status: :ok
@@ -12,5 +10,21 @@ class BuildingsController < ApplicationController
         render json: building, status: :ok
     end
 
+    def create
+        building = Building.create(building_params)
+        render json: building, status: :created
+    end
+
+    def destroy
+        building = Building.find(params[:id])
+        building.destroy
+        head :no_content
+    end
    
+
+    private
+
+    def building_params
+        params.permit(:name, :code)
+    end
 end
