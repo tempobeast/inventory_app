1c6a68e4f495d64453597ad1e815355197997a3f
Zach <shaw.zacht@gmail.com>
2023-11-19 11:58:06 -0800

    create model

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,8 +1,19 @@
 class TransfersController < ApplicationController
 
     def create 
-        computer = computer.find_by(id: params[:computer_id])
-        computer.building_id = params[:to]
+        byebug
+        transfer = Transfer.create(transfer_params)
+        computer = Computer.find(transfer[:computer_id])
+        byebug
+        new_building = Building.find_by(code: transfer[:to])
+        computer.update(building_id: new_building[:id])
+        byebug
         render json: computer, status: :ok
+    end
+
+    private 
+
+    def transfer_params 
+        params.permit(:to, :from, :computer_id, :transfer_type)
     end
 end
