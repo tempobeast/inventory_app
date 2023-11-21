c909a5040d0ec427519d3150b831eed427120d5d
Zach <shaw.zacht@gmail.com>
2023-11-20 20:26:08 -0800

    remove byebugs

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,13 +1,10 @@
 class TransfersController < ApplicationController
 
     def create 
-        byebug
         transfer = Transfer.create(transfer_params)
         computer = Computer.find(transfer[:computer_id])
-        byebug
         new_building = Building.find_by(code: transfer[:to])
         computer.update(building_id: new_building[:id])
-        byebug
         render json: computer, status: :ok
     end
 
