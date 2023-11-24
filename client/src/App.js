3a58c9b19633ef43054f98e9d17bf78785ed2d0b
Zach <shaw.zacht@gmail.com>
2023-11-24 10:18:14 -0800

    update title and init computer fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [computers, setComputers] = useState([])  
+
+  useEffect(() => {
+    fetch('computers')
+    .then((res) => res.json()
+    .then((data) => console.log(data))
+    )
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Inventory App</h1>
     </div>
   );
 }
