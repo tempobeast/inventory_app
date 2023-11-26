6a4ccea2cd9ff72e03c201f48a6c76adc3d4e14f
Zach <shaw.zacht@gmail.com>
2023-11-25 21:50:01 -0800

    show all computers associated with selected building

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import React, { useEffect, useState } from 'react'
 
 function App() {
   const [buildings, setBuildings] = useState([]);
-  const [selectedBuilding, setSelectedBuilding] = useState([]);
+  const [selectedBuilding, setSelectedBuilding] = useState({
+    name: "",
+    code: "",
+    computers: []
+  });
 
   useEffect(() => {
     fetch('buildings')
@@ -16,16 +20,36 @@ function App() {
   const buildingsToRender = buildings.map((building) => (
     <option key={building.id} value={building.name}>{building.name}</option>
   ))
+  
+  const computersToRender = selectedBuilding.computers.map((computer) => (
+    <li key={computer.id}>
+      <p>{computer.asset_tag}</p>
+      <p>{computer.model}</p>
+      <p>{computer.serial_number}</p>
+    </li>
+  ))
+
+  function handleBuildingChange(e) {
+    setSelectedBuilding(buildings.find((building) => building.name === e.target.value))
+  }
 
   return (
     <div className="App">
       <h1>Inventory App</h1>
-      <select onChange={(e) => setSelectedBuilding(e.target.value)}>
+      <select onChange={handleBuildingChange}>
         {buildingsToRender}
       </select>
+      {selectedBuilding ? 
       <ul>
-
+        <li>
+          <p>Tag Number</p>
+          <p>Model</p>
+          <p>Serial Number</p>
+        </li>
+        {computersToRender}
       </ul>
+      : null
+    }
     </div>
   );
 }
