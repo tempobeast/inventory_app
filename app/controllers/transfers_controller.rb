9e568654825c71203990d88cc88a80e60f7ba1a2
Zach <shaw.zacht@gmail.com>
2023-12-04 14:00:45 -0800

    render old and new buildings

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -3,9 +3,10 @@ class TransfersController < ApplicationController
     def create 
         transfer = Transfer.create(transfer_params)
         computer = Computer.find(transfer[:computer_id])
+        old_building = computer.building
         new_building = Building.find_by(code: transfer[:to])
         computer.update(building_id: new_building[:id])
-        render json: computer, status: :ok
+        render json: [old_building, new_building], status: :ok
     end
 
     private 
